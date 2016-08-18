eb308a5cc9b33012ab7e0fecb67c4bac2c02e20b
Abrie van Aardt <linkin1903@gmail.com>
2016-08-18 22:00:02 +0200

    Corrected pattern input delimiter

--- Implementation/NeutralityStudy/src/main/java/ac/up/cos700/neutralitystudy/neuralnet/training/BackPropogation.java
+++ Implementation/NeutralityStudy/src/main/java/ac/up/cos700/neutralitystudy/neuralnet/training/BackPropogation.java
@@ -1,6 +1,7 @@
 package ac.up.cos700.neutralitystudy.neuralnet.training;
 
 import ac.up.cos700.neutralitystudy.neuralnet.IFFNeuralNet;
+import ac.up.cos700.neutralitystudy.neuralnet.Neuron;
 import java.util.Random;
 
 /**
@@ -11,13 +12,30 @@ public class BackPropogation implements IFFNeuralNetTrainer {
 
     @Override
     public void train(IFFNeuralNet network) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    private void initialise(IFFNeuralNet network){
+        initialise(network);
+        double networkError;
+        
+        do{
+            
+        } while (networkError <= errorDelta);
+        
         
     }
-    
-    private Random seed = new Random(System.nanoTime());
 
+    private void initialise(IFFNeuralNet network) {
+        Neuron[][] layers = network.getNetworkLayers();
+        for (int i = 0; i < layers.length; i++) {
+            for (int j = 0; j < layers[i].length; j++) {
+                int fanin = layers[i][j].getWeightCount();
+                double range = 1.0 / Math.sqrt(fanin);
+                for (int k = 0; k < fanin; k++) {
+                    layers[i][j].setWeight(k,
+                            rand.nextDouble() * 2 * range - range);
+                }
+            }
+        }
+    }
+
+    private Random rand = new Random(System.nanoTime());
+    private double errorDelta = 0.0001;
 }
